50dbfbdafd3d628983d5b508b2b0e85a6c49130e
Ricardo Santiago Morais <santhiagoxp7@gmail.com>
2020-01-21 18:36:07 -0300

    Correcao de urls para api

--- src/app/services/env.service.ts
+++ src/app/services/env.service.ts
@@ -5,11 +5,11 @@ import { Injectable } from '@angular/core';
 })
 export class EnvService {
 
- API_URL = 'http://localhost:9003/';
- API_URL_TAREFA = 'http://localhost:9003/tarefa/';
+//  API_URL = 'http://localhost:9003/';
+//  API_URL_TAREFA = 'http://localhost:9003/tarefa/';
 
-  // API_URL = 'http://177.126.81.87:8089/sgc/';
-  // API_URL_TAREFA = 'http://177.126.81.87:8089/sgc/tarefa/';
+  API_URL = 'http://177.126.81.87:8089/sgc/';
+  API_URL_TAREFA = 'http://177.126.81.87:8089/sgc/tarefa/';
 
   constructor() { }
 }
